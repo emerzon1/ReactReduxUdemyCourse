81517e9d22b272bbb1f25327a7fbf7d8f043b944
Evan Merzon <evandaniel160@yahoo.com>
2020-08-06 14:41:56 -0400

    Access images from api

--- fetching-pictures/src/Components/SearchBar.js
+++ fetching-pictures/src/Components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    /*constructor(props){
+        super(props);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }*/ // ONE FIX FOR this error, also can change function into arrow function
     state = {term: ''}
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onClick={this.onInputClick} onChange={(e) => this.setState({term: e.target.value})} />
