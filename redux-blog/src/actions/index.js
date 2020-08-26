5a91e9a0bf84a6ccbd789b100680524ad2c764f3
Evan Merzon <evandaniel160@yahoo.com>
2020-08-26 09:26:13 -0400

    finish redux-blog

--- redux-blog/src/actions/index.js
+++ redux-blog/src/actions/index.js
@@ -1,8 +1,25 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-
+import _ from "lodash";
+/* export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts()); 
+        const userIds = _.uniq(_.map(getState().posts, 'userid'))
+        userIds.forEach(user => dispatch(fetchUser(user)))
+    }
+} */
 export const fetchPosts = () => {
-    return async dispatch => {
+    return async (dispatch) => {
         const response = await jsonPlaceholder.get("/posts");
-        dispatch({type:'FETCH_POSTS', payload: response.data})
+        dispatch({ type: "FETCH_POSTS", payload: response.data });
     };
 };
+
+export const fetchUser = (user) => {
+    return (dispatch) => {
+        _fetchUser(user, dispatch);
+    };
+};
+const _fetchUser = _.memoize(async (user, dispatch) => {
+    const response = await jsonPlaceholder.get("/users/" + user);
+    dispatch({ type: "FETCH_USER", payload: response.data });
+});
